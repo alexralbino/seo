5e8d693f79aa8438f76baf395e2baff68c755026
Carlos Tamagawa <cstamagawa@gmail.com>
2016-09-12 18:13:32 +0000

    implementação title com foreign keys

--- src/SeoTrait.php
+++ src/SeoTrait.php
@@ -23,9 +23,35 @@ trait SeoTrait
 
     public function getSeoTitleAttribute()
     {
-        return $this[$this->seomap['title']];
+        $from = $this->seomap['title'];
+        $source = array_map([$this, 'generateSourceSeo'], (array)$from);
+        return join($source, ' ');
     }
 
+        /**
+     * Get value for seo.
+     *
+     * @param string $key
+     * @return string|null
+     */
+    private function generateSourceSeo($key)
+    {
+        if (isset($this->{$key})) {
+            return $this->{$key};
+        }
+
+        $object = $this;
+        foreach (explode('.', $key) as $segment) {
+            if (!is_object($object) || !$tmp = $object->{$segment}) {
+                return null;
+            }
+
+            $object = $object->{$segment};
+        }
+
+        return $object;
+    }
+    
     public function getSeoDescriptionAttribute()
     {
         return $this[$this->seomap['description']];
